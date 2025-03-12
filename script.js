461623805c7d3a6c2dd8e16c59048cbcbda2766a
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-03-12 21:26:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log('Saidazim jonkaxon')
+// alert("Превет кизлар")
+
+var a = 5
+console.log(a)
+
+let nameK = "Kamron chika"
+// console.log(nameK)
+
+const ibo ='Qovn bu Ibo'
+// alert(ibo)
+
+
+ let info = prompt('Укажите имя ')
+ console.log(info)
+
+
+ let age = prompt('Ведите свой возраст')
+ console.log(age)
+
+ let arr = prompt('Кгде вы живёте')
+ console.log(arr)
+
+ let aff = prompt('Yoqatirgan Faningizni yozing')
+ console.log(aff)
+
+ let dee = prompt('Yashaydigan tumaningizni kritng')
+ console.log(dee)
+
+ alert(`Меня зовут ${info }`)
+
+
+
+
+
+
+ 
